20df1e3d4af29caf93216fa1d3c93dc9c1fee72e
Aditha Doratiyawa <indegamedev@gmail.com>
2021-10-08 00:38:59 +1100

    test view changes

--- app/src/views/Tests.vue
+++ app/src/views/Tests.vue
@@ -18,38 +18,82 @@
             hover:scale-105
           "
           type="button"
-        >
-          Schedule Test
-        </button>
-        <button
-          class="
-            rounded
-            shadow-md
-            py-2
-            px-4
-            bg-blue-700
-            text-gray-200 text-xl
-            font-bold
-            h-1/4
-            transition
-            transform
-            hover:scale-105
-          "
-          type="button"
           @click="$router.push({ name: 'AddTest' })"
         >
           Add Test
         </button>
       </div>
     </div>
-    <div class="rounded shadow-md bg-gray-700 py-2 px-4">
-      <h2 class="text-2xl text-gray-200 font-bold">Schedule</h2>
+    <div class="">
+      <div
+        class="
+          flex
+          rounded
+          shadow-md
+          w-full
+          py-2
+          px-4
+          bg-gray-900
+          text-gray-100
+        "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-10 w-10 mr-4 text-gray-800"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <input
+          class="bg-gray-900 w-full"
+          placeholder="Quick Search.."
+          v-model="searchTerms"
+          v-on:keydown.enter="search"
+        />
+      </div>
+    </div>
+
+    <div class="rounded shadow-md bg-gray-700 py-2 px-4 flex justify-between">
+      <h2 class="text-2xl text-gray-200 font-bold self-center">Schedule</h2>
+      <button
+        class="
+          rounded
+          shadow-md
+          py-2
+          px-4
+          bg-gray-900
+          text-gray-200 text-xl
+          font-bold
+          transition
+          transform
+          hover:scale-105
+        "
+        type="button"
+      >
+        Schedule Test
+      </button>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const searchTerms = ref("");
+    return {
+      searchTerms,
+    };
+  },
+});
 </script>
 
 <style></style>
