a63a46d0d5e8ab13f73bfea6a183d3bbabdf6e82
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2021-10-16 13:48:25 +1100

    Create studentParts_model.ts

---
+++ app/src/backend/students/studentParts_model.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model, Optional, Sequelize } from "sequelize";
+
+interface studentPartsAttributes {
+    studentPartId: number;
+    partId: number;
+    studentId: number;
+    workEventId: number;
+}
+
+type studentPartsCreationAttributes = Optional<studentPartsAttributes, "studentPartId">; 
+export class studentParts extends Model<studentPartsAttributes, studentPartsCreationAttributes> {}
+
+export function model(sequelize: Sequelize): void {
+    studentParts.init(
+        {
+            studentPartId: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            partId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            studentId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            workEventId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            }
+        },
+        {
+            sequelize,
+            modelName: "studentParts",
+        }
+    );
+}
