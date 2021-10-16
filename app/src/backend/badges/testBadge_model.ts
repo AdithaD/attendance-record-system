99908c4e66dc257b394c11f29b9beb99be16204d
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2021-10-16 12:23:47 +1100

    Create testBadge_model.ts

---
+++ app/src/backend/badges/testBadge_model.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model, Optional, Sequelize } from "sequelize";
+import { Test } from "./test_model";
+import { Topic } from "./topic_model";
+
+interface testBadgeAttributes {
+    testBadgeId: number;
+    topicId: number;
+    testId: number;
+    name: string;
+    isOptional: boolean;
+}
+
+type testBadgeCreationAttributes = Optional<testBadgeAttributes, "testBadgeId">;
+export class testBadge extends Model<testBadgeAttributes, testBadgeCreationAttributes> {}
+
+export function model(sequelize: Sequelize): void {
+    testBadge.init(
+        {
+            testBadgeId: {
+                type: DataTypes.INTEGER,
+             primaryKey: true,
+                autoIncrement: true,
+            },
+            topicId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            testId:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            isOptional: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: "testBadge",
+        }
+    );
+}
+
+export function relations(): void {
+    testBadge.hasMany(Test, {
+        foreignKey: {name: "testBadgeId"},
+        constraints: false,
+    });
+    testBadge.belongsTo(Test, {foreignKey: { name: "testID"}});
+}
