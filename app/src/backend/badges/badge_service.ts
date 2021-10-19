da6c1bb36ba171b67ddb33ba29c97f7c0d1c827a
KyteEvans <z5317466@unsw.edu.au>
2021-10-19 11:55:34 +1100

    created getLeaderboard() method
    thx Dora

--- app/src/backend/badges/badge_service.ts
+++ app/src/backend/badges/badge_service.ts
@@ -1,4 +1,6 @@
 import TopicData from "@/store/Topic";
+import { Student } from "../students/student_model";
+import { Badge } from "./badge_model";
 import { Part } from "./part_model";
 import { Test } from "./test_model";
 import { TestSchedule } from "./test_schedule_model";
@@ -34,17 +36,17 @@ export async function addTest(
   });
 }
 
-// export enum Type {
-//   A,
-//   S,
-//   C,
-// }
+export enum Type {
+  A,
+  S,
+  C,
+}
 
-// export enum Tier {
-//   Diamond,
-//   Platnium,
-//   Lithium,
-// }
+export enum Tier {
+  Diamond,
+  Platinum,
+  Lithium,
+}
 
 // export function addBadge(
 //   name: String,
@@ -58,6 +60,8 @@ export class StudentBadgeCount {
   public diamond: number;
   public platinum: number;
   public lithium: number;
+  public static readonly platinumWeightingNumber: number = 2;
+  public static readonly lithiumWeightingNumber: number = 3;
 
   constructor(
     studentId: number,
@@ -71,6 +75,64 @@ export class StudentBadgeCount {
     this.lithium = lithium;
   }
   public getValue(): number {
-    return 0;
+    return (
+      this.diamond +
+      this.platinum * StudentBadgeCount.platinumWeightingNumber +
+      this.lithium * StudentBadgeCount.lithiumWeightingNumber
+    );
   }
 }
+export async function getLeaderboard(
+  studentId: number,
+  diamond: number,
+  platinum: number,
+  lithium: number
+): Promise<StudentBadgeCount[]> {
+  const students = await Student.findAll({ include: Badge });
+  const sbc = [] as StudentBadgeCount[];
+  students.forEach((student) => {
+    const badgeCounts = [0, 0, 0];
+    const badges = student.get("Badges") as Badge[];
+    badges.forEach((badge) => {
+      switch (badge.get("badgeTier") as string) {
+        //diamond for base value
+        case Tier.Diamond.toString():
+          badgeCounts[0] += 1;
+          break;
+        //platinum in middle value of array
+        case Tier.Platinum.toString():
+          badgeCounts[1] += 1;
+          break;
+        //lithium as top value within array
+        case Tier.Lithium.toString():
+          badgeCounts[2] += 1;
+          break;
+
+        default:
+          break;
+      }
+    });
+    sbc.push(
+      new StudentBadgeCount(
+        student.get("studentId") as number,
+        badgeCounts[0],
+        badgeCounts[1],
+        badgeCounts[2]
+      )
+    );
+  });
+  sbc.sort((a, b) => {
+    if (a.getValue() < b.getValue()) return 1;
+    else {
+      if (a.getValue() > b.getValue()) return -1;
+      else {
+        return 0;
+      }
+    }
+  });
+  return sbc;
+}
+
+//display each student with total number of badges
+
+//findAndCountAll method for the array
--- app/src/backend/students/student_service.ts
+++ app/src/backend/students/student_service.ts
@@ -4,6 +4,8 @@ import { Badge } from "../badges/badge_model";
 import { Sequelize } from "sequelize/types";
 import sequelize from "sequelize/types/lib/sequelize";
 import { TestSchedule } from "../badges/test_schedule_model";
+import { StudentBadgeCount } from "../badges/badge_service";
+import { any } from "sequelize/types/lib/operators";
 
 export async function getStudent(id: number): Promise<Student> {
   const student = await Student.findOne({ where: { studentId: id } });
@@ -29,6 +31,26 @@ export async function getAllStudents(): Promise<Student[]> {
   const students = await Student.findAll();
   return students;
 }
+
+export async function getLeaderboard(
+  studentId: number,
+  diamond: number,
+  platinum: number,
+  lithium: number
+): Promise<any> {
+  const studentLeaderboard = await StudentBadge.findOne({
+    where: { studentId: "studentId" },
+  });
+  if (studentLeaderboard === null) {
+    console.log("Not found!");
+  } else {
+    console.log(studentLeaderboard instanceof StudentBadge);
+  }
+}
+
+//display each student with total number of badges
+
+//findAndCountAll method for the array
 
 export async function getBadgesForStudents(): Promise<void> {
   const bfs = await Badge.findByPk("studentId");
